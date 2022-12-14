bcd38d53471dfae28537645ab14597b79207ec14
vikash <shaluraj.rc@gmail.com>
2022-12-14 10:50:12 -0800

    done

---
+++ src/main/java/learningGitCommands/learningGitCommand/App.java
@@ -0,0 +1,13 @@
+package learningGitCommands.learningGitCommand;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
