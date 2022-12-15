37f1b04ea66c492dbee4e4eb832953691de1ea70
vikash <shaluraj.rc@gmail.com>
2022-12-15 12:27:56 +0530

    finaldone

---
+++ src/test/java/learningGitCommands/learningGitCommand/Appium.java
@@ -0,0 +1,10 @@
+package learningGitCommands.learningGitCommand;
+
+public class Appium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/learningGitCommands/learningGitCommand/Selenium.java
@@ -0,0 +1,21 @@
+package learningGitCommands.learningGitCommand;
+
+import org.testng.annotations.Test;
+
+public class Selenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		@Test
+		public void seleniumLearn() {
+			System.out.print("this is a selenium items");
+		}
+		@Test
+		public void seleniumInterview() {
+			System.out.print("this is a interview items");
+		}
+
+	}
+
+}
