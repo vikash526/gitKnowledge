58c722044fed9396a696be7e67014b4d6bcaa3dc
vikash <shaluraj.rc@gmail.com>
2022-12-15 11:46:38 +0530

    gitknowledge updated

--- src/test/java/learningGitCommands/learningGitCommand/Appium.java
+++ src/test/java/learningGitCommands/learningGitCommand/Appium.java
@@ -5,6 +5,10 @@ public class Appium {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print(false);
+		System.out.print(false);
+		System.out.print(false);
+		System.out.print(false);
+		System.out.print(false);
 
 	}
 
